5f5fc43ef4b7e07ed438cd7cc89f1ce8252e42ea
Gizele-Aydi <aydigizele@tbs.u-tunis.tn>
2024-12-05 19:04:20 +0100

    data cleaning again and again

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -10,8 +10,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         DataUploadController controller = new DataUploadController();
-
         Object[][] table = null;
+
         while (true) {
             List<List<String>> data = controller.getData();
             if (!data.isEmpty()) {
